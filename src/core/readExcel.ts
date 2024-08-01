3c4ae0d234785b2136eab586d25b9256caf72581
deofss <florin.criveanu@distributieoltenia.ro>
2024-08-01 16:51:25 +0300

    added exceptions to index mismatch

--- src/core/readExcel.ts
+++ src/core/readExcel.ts
@@ -2,13 +2,13 @@ import * as XLSX from "xlsx";
 import { useReports } from "./useReports";
 import { useTotals } from "./useTotals";
 import { useCorrect } from "./useCorrect";
-import {runCheckIndexMismatch} from './runCheckIndexMismatch'
+import { runCheckIndexMismatch } from "./runCheckIndexMismatch";
 
 export const readExcel = async (file: any) => {
   return new Promise((res, rej) => {
     const reader = new FileReader();
 
-    reader.onload =async (e: any) => {
+    reader.onload = async (e: any) => {
       if (!e.target.result) return;
       const data = e.target.result;
       const sheetNames = XLSX.readFile(data).SheetNames;
@@ -38,7 +38,7 @@ export const readExcel = async (file: any) => {
       parsedTotalSheet?.splice(0, 1);
 
       const reports = useReports(parsedReportSheet);
-      runCheckIndexMismatch(reports,file.name)
+      runCheckIndexMismatch(reports as any[], file.name);
       const totals = useTotals(parsedTotalSheet);
       const corrected = useCorrect(
         reports as any[],
--- src/core/runCheckIndexMismatch.ts
+++ src/core/runCheckIndexMismatch.ts
@@ -16,21 +16,23 @@ const chechIndexMismatch= (data:any[],consumptionPlace:string,fileName:string)=>
     if(!consumptionPlace) return;
     const filteredArray = data?.filter((item)=>item?.loc_de_consum === consumptionPlace )
     const eaArray = filteredArray?.filter((item)=>item?.cadran_registru==="EA")
-    const total = filteredArray?.find((item)=>item?.isTotal === true)?.total_cantitate_energie_activa
-    
+    const totalArray = filteredArray?.filter((item)=>item?.isTotal === true)
+    console.log(totalArray)
+    const total= totalArray?.reduce((accum, current)=> accum + current?.total_cantitate_energie_activa,0)
+    console.log(total)
     let totalIndex=0
     for(let singleLine of eaArray){
         const indexVechi = singleLine?.index_vechi;
         const indexNou = singleLine?.index_now;
         const pierderi = singleLine?.pierderi_dat_pd_pm;
         const corectii = singleLine?.alte_corectii
-
+        const constanta = singleLine?.constanta
 
         const cantitate = Number(Number(indexNou) - Number(indexVechi) + Number(pierderi) + Number(corectii))
-        totalIndex = cantitate + totalIndex
+        totalIndex = Number(cantitate*constanta) + Number(totalIndex)
     }
 
-    if(Number(total - totalIndex) > 1 ||Number(total - totalIndex)< -1){
+    if(Number(total - totalIndex) >=1 ||Number(total - totalIndex)<= -1){
      store.dispatch(
         addSingleValue([consumptionPlace, fileName, "detectat_inconsistenta_index"])
       );
